508e394b12664523476830ff32c831849514865d
Anabel <assannanabel@gmail.com>
2023-04-25 11:53:02 -0300

    finalizado

---
+++ Enum.ts
@@ -0,0 +1,9 @@
+export enum EnumSubjects {
+    Matematica = "matematica",
+     Lengua = "lengua", 
+     Sociales = "sociales", 
+     Naturales = "naturales", 
+     Ingles = "ingles", 
+     EdFisica = "ed.fisica"
+  }
+
